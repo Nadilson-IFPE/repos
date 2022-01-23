0a0c9fa9dc42abb1026a1b5136e6587db03bdf6d
Nadilson <nadilson@protonmail.com>
2022-01-23 05:56:58 -0300

    Estilizando a página Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,23 @@
 import React from "react";
-
-import { Title } from "./styles";
+import {FaGithub, FaPlus} from 'react-icons/fa';
+import { Container, Form, SubmitButton } from "./styles";
 
 export default function Main() {
-  return <Title>
-             Projeto
-         </Title>;
+    return (
+        <Container>
+            <h1>
+                <FaGithub size={25} />
+                Meus Repositórios
+            </h1>
+
+            <Form>
+                <input type="text" placeholder="Adicionar Repositórios" />
+
+                <SubmitButton>
+                    <FaPlus color="#FFF" size={14} />
+                </SubmitButton>
+            </Form>
+
+        </Container>
+  )
 }
