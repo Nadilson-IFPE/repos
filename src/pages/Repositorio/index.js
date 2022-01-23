af70c1f302c1f09cab7fe6324160e62d6d449804
Nadilson <nadilson@protonmail.com>
2022-01-23 13:06:01 -0300

    Carregando detalhes

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import api from "../../services/api";
+import { Container } from "../Main/styles";
 
 export default function Repositorio() {
-  const { repositorio } = useParams();
+    const { repositorio } = useParams();
+
+    const [repository, setRepository] = useState({});
+    const [issues, setissues] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function load() {
+            const nomeRepo = decodeURIComponent(repositorio);
+
+         const [repositorioData, issuesData] = await Promise.all([
+                api.get(`/repos/${nomeRepo}`), 
+                api.get(`/repos/${nomeRepo}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page: 5
+                    }
+                })
+            ]);
+
+
+          //  console.log(repositorioData.data);
+          //  console.log(issuesData.data);
+          
+          setRepository(repositorioData.data);
+          setissues(issues.data);
+          setLoading(false);
+
+        }
+
+        load();
+
+    }, []);
+
   return (
-    <>
-      <h1 style={{ color: "#FFF" }}>Repositórios</h1>
-      <span style={{ color: "#FFF" }}>{decodeURIComponent(repositorio)}</span>
-    </>
+    <Container>
+         
+    </Container>
   );
 }
